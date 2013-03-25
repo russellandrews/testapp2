0d1303f1051636b1b5c29821f9dde58ced9ae829
russellandrews <rwandrews62@gmail.com>
2013-03-25 00:17:44 -0400

    this adds everything that comes with rails.

---
+++ testapp2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp2::Application.config.secret_token = 'f9330e43ef6a9853d47e99331d9bf5215429c6fc002efea56ac93c1ef069638ed40a30e362f595c0f42bb206bab9f84b5fb5e798300999bf4d2f0680795c9cb3'
